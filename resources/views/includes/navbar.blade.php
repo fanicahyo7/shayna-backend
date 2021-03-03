48a9034e09c31eb0d7b25811df2b10ea2f0d94f6
fanicahyo7 <fanidc7@gmail.com>
2021-03-03 21:06:40 +0700

    ui auth

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -16,7 +16,8 @@
                 </a>
 
                 <div class="user-menu dropdown-menu">
-                    <a class="nav-link" href="#"><i class="fa fa-power -off"></i>Logout</a>
+                    <a class="nav-link" href="{{ route('logout') }}"onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();"><i class="fa fa-power -off"></i>{{ __('Logout') }}</a>
                 </div>
             </div>
 
